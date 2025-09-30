2226bcf46c93fbb9279310c64c1ad7a205b23a69
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-09-30 20:43:22 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,8 @@ const Navbar = ({ activeElement, handleImageUpload, handleActiveElement,imageInp
     (activeElement && activeElement.value === value) ||
     (Array.isArray(value) && value.some((val) => val?.value === activeElement?.value));
 
+
+  
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
