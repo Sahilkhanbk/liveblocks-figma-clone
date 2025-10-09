984b243370f28a1f8d39c141d7e2319cd12155fa
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-10-10 01:01:22 +0530

    Update useInterval.ts

--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -6,7 +6,6 @@ import { useEffect, useRef } from "react";
 
 
 
-
 export default function useInterval(callback: () => void, delay: number) {
   const savedCallback = useRef<() => void>(callback);
 
