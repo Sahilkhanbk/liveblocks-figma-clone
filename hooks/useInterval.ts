7e64319bfb73f263991c8ceb7de0389065f85713
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-10-14 07:42:47 +0530

    Update useInterval.ts

--- hooks/useInterval.ts
+++ hooks/useInterval.ts
@@ -10,7 +10,6 @@ export default function useInterval(callback: () => void, delay: number) {
     savedCallback.current = callback;
   }, [callback]);
 
-  
   // Set up the interval.
   useEffect(() => {
     const tick = () => {
